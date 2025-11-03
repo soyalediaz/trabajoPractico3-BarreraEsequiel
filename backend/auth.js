dda931bec3e4a29e3a898d3b9938f8422997065f
Alejandro Diaz <alekey012@gmail.com>
2025-11-03 20:22:43 -0300

    intentando hacer la autenticacion

--- backend/auth.js
+++ backend/auth.js
@@ -1,5 +1,9 @@
 import express from "express";
 import passport from "passport";
+import bcrypt from "bcrypt";
+import { body } from "express-validator";
+import { db } from "./db.js";
+import { verificarValidaciones } from "./validaciones.js";
 import { Strategy, ExtractJwt } from "passport-jwt";
 
 const router = express.Router();
@@ -20,4 +24,36 @@ export function authConfig() {
 }
 
 
+export const verficarAutenticacion = passport.authenticate("jwt", {
 
+    session: false
+});
+
+router.post(
+    "/registro",
+    body("email").isEmail().normalizeEmail(),
+    body("nombre").isLength({ min: 2, max: 50 }).trim(),
+    body("password").isStrongPassword({
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 0,
+        minNumbers: 1,
+        minSymbols: 0
+    }),
+
+    verificarValidaciones,
+
+    async (req, res) => {
+        const hashedPassword = await bcrypt.hash(req.body.password, 12);
+
+        await db.execute(
+            "INSERT INTO usuarios (email, nombre, password) VALUES (?, ?, ?)",
+            [email, nombre, hashedPassword]
+        );
+
+        res.status(201).json({
+            success: true,
+            message: "Usuario registrado correctamente"
+        });
+    }
+);
